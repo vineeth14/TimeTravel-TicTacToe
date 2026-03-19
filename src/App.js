ea16904f240918a7bc09e206d819624e703e0444
Vineeth <vineethrajesh14@gmail.com>
2026-03-19 09:44:46 -0400

    time travel

--- src/App.js
+++ src/App.js
@@ -51,16 +51,20 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
-	const currentSquares = history[history.length - 1];
+	const [currentMove, setCurrentMove] = useState(0);
+	const xIsNext = currentMove % 2 === 0;
+	const currentSquares = history[currentMove];
 
 	function handlePlay(nextSquares) {
-		setHistory([...history, nextSquares]);
-		setXIsNext(!xIsNext);
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
 	}
 
-	function jumpTo(nextMove) {}
+	function jumpTo(nextMove) {
+		setCurrentMove(nextMove);
+	}
 
 	const moves = history.map((squares, move) => {
 		let description;
@@ -70,7 +74,7 @@ export default function Game() {
 			description = "Go to game start";
 		}
 		return (
-			<li>
+			<li key={move}>
 				<button onClick={() => jumpTo(move)}>{description}</button>
 			</li>
 		);
